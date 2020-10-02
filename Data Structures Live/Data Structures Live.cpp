0715e53e7e8f34e7b51ee79364344ade78784ccb
Pekka Aleksi Kasa <github@kasa.site>
2020-10-02 15:30:20 +0300

    Add reallocation code for the changing stack size.

--- Data Structures Live/Data Structures Live.cpp
+++ Data Structures Live/Data Structures Live.cpp
@@ -1,14 +1,13 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include <iostream>
+#include <cstdio> 
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace DataStructuresLive
 {
-
-
-
-
 	TEST_MODULE_INITIALIZE(ModuleInitialize)
 	{
 		Logger::WriteMessage("In Module Initialize");
@@ -21,17 +20,57 @@ namespace DataStructuresLive
 
 
 	class MyStack {
-
-
 		size_t size = 0;
-		int array[16];
+		size_t max_size;
+
+		int * array;
 		bool empty = true;
 
 	public:
+
+		MyStack(size_t max_size=16): max_size(max_size)
+		{
+			this->array = new int[this->max_size];
+		}
+
+		~MyStack() { delete[] this->array; this->array = nullptr; }
+
 		size_t getSize() { return size;  }
+
+		void resize() {
+			
+			size_t MAX_SIZE = 2 << 15;
+
+			size_t new_size = this->max_size * 2 > MAX_SIZE ? MAX_SIZE : this->max_size*2;
+
+			char msg[256];
+			std::sprintf(msg, "Running resize from %d to %d", this->max_size, new_size);
+
+			Logger::WriteMessage(msg);
+
+			int* tmp = new int[new_size];
+
+			if (tmp != nullptr) {
+				for (size_t a = 0; a < this->max_size; a++) {
+					tmp[a] = this->array[a];
+				}
+
+				delete[] this->array;
+				this->max_size = new_size;
+				this->array = tmp;
+			}
+		}
+
+
 		void push(int item) {
+
+			if (this->size == this->max_size) {
+				resize();
+			} 
+			
 			array[size] = item;
 			size++;
+
 		}
 
 		int pop() {
@@ -82,13 +121,28 @@ namespace DataStructuresLive
 			Assert::ExpectException<std::out_of_range>(func);			
 		}
 
-		TEST_METHOD(PushTenElementsToEmptyStack) {
+		TEST_METHOD(PushSmallAmountOfElementsToEmptyStack) {
 
 			MyStack stack;
 
 			size_t const  K = 3;
 
 			for (int i=0; i< K; i++)
+				stack.push(i);
+
+			auto size = stack.getSize();
+
+			Assert::AreEqual(K, size);
+		}
+
+
+		TEST_METHOD(PushLargeAmountOfElementsToEmptyStack) {
+
+			MyStack stack;
+
+			size_t const  K = 1025;
+
+			for (int i = 0; i < K; i++)
 				stack.push(i);
 
 			auto size = stack.getSize();
@@ -109,8 +163,6 @@ namespace DataStructuresLive
 			for (int i = 0; i < 3; i++)
 				Assert::AreEqual(K - (i + 1), stack.pop());
 		}
-
-
 
 	};
 }
